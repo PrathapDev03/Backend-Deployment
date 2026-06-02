38bd3b269008f79cb3af625ea2a0d90cd7ebd55b
PrathapDev03 <prathap03.p@gmail.com>
2026-06-02 12:14:13 +0530

    Connect Elastic Beanstalk to RDS

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
