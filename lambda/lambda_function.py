06772525d49c2648071d542869135c6b0e2bcd49
PrathapDev03 <prathap03.p@gmail.com>
2026-06-03 14:59:07 +0530

    Testing Jenkins Lambda deployment

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -100,7 +100,7 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "body": json.dumps({
-                "message": "Employee Updated Successfully"
+                "message": "Updated by Jenkins Pipeline"
             })
         }
 
